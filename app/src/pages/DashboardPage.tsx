d5dc299be7fdd18e4d5d7a0a64b7eaf77b8bb313
Sofia Rothman <sofia.rothman87@gmail.com>
2026-03-16 16:47:29 +0100

    feat: fix TSC issues

--- app/src/pages/DashboardPage.tsx
+++ app/src/pages/DashboardPage.tsx
@@ -1,20 +1,20 @@
-//import { useState } from "react"
-/* import ParentView from "../views/ParentView"
-import KidsView from "../views/KidsView" */
+import { useState } from "react"
+import ParentView from "../views/ParentView"
+import KidsView from "../views/KidsView" 
 
 const DashboardPage = () => {
- // const [userRole, setUserRole] = useState<"parent" | "kid">("kid")
+ const [userRole, setUserRole] = useState<"parent" | "kid">("kid")
 
   return (
     <div>
       
-      {/* <button
+       <button
         onClick={() => setUserRole(userRole === "kid" ? "parent" : "kid")}
       >
         Växla till {userRole === "kid" ? "Förälder" : "Barn"}
       </button>
 
-      {userRole === "parent" ? <ParentView /> : <KidsView />} */}
+      {userRole === "parent" ? <ParentView /> : <KidsView />} 
     </div>
   )
 }
